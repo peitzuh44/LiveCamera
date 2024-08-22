c1ef571991766344cf2256ec45d470adfcf4d5fa
Pei <peitzuh44@berkeley.edu>
2024-08-22 19:25:07 +0800

    Initial Commit

---
+++ LiveCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveCamera
+//
+//  Created by Pei-Tzu Huang on 2024/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveCamera/LiveCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCameraApp.swift
+//  LiveCamera
+//
+//  Created by Pei-Tzu Huang on 2024/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LiveCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
