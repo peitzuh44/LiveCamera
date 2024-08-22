3597c735fe5047f441abcda7ad422241297846e7
Pei <peitzuh44@berkeley.edu>
2024-08-22 20:07:15 +0800

    Initial Commit

---
+++ LiveCamera/FrameHandler.swift
@@ -0,0 +1,8 @@
+//
+//  FrameHandler.swift
+//  LiveCamera
+//
+//  Created by Pei-Tzu Huang on 2024/8/22.
+//
+
+import Foundation
---
+++ LiveCamera/FrameView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameView.swift
+//  LiveCamera
+//
+//  Created by Pei-Tzu Huang on 2024/8/22.
+//
+
+import SwiftUI
+
+struct FrameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameView()
+}
